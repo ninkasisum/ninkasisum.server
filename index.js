4138e8378c375978d993f7cdc92069b30fd3b992
Diogo Junqueira Geraldo <diogojunqueirageraldo@gmail.com>
2020-06-29 23:35:45 -0300

    [hot fix] NOW IT GO

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/calls',express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.get('/', (req, res) => res.sendFile(__dirname + '\\views\\pages\\index.html'));
 app.get('/login', (req, res) => res.sendFile(__dirname + '\\views\\pages\\login.html'));
 
