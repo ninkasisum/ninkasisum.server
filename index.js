36008d9c4385f04e15d5a3bbfbac4b1a2588f035
Diogo Junqueira Geraldo <diogojunqueirageraldo@gmail.com>
2020-06-29 23:17:20 -0300

    [add] heroku setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/calls',express.static(path.join(__dirname, '/public')));
+app.get('/', (req, res) => res.sendFile(__dirname + '\\views\\pages\\index.html'));
+app.get('/login', (req, res) => res.sendFile(__dirname + '\\views\\pages\\login.html'));
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
---
+++ public/scripts/index.js
@@ -0,0 +1,36 @@
+//Autenticar Google Account
+function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+}
+
+//facebook
+window.fbAsyncInit = function() {
+    FB.init({
+    appId      : '{274401163831368}',
+    cookie     : true,
+    xfbml      : true,
+    version    : '{7.0}'
+    });
+    
+    FB.AppEvents.logPageView();   
+    
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
