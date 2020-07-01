e86fc48a9a0b3310819bb299b8b0105bf686bf31
Diogo Junqueira Geraldo <diogojunqueirageraldo@gmail.com>
2020-07-01 11:51:56 -0300

    [login] feature

--- index.js
+++ index.js
@@ -1,52 +1,40 @@
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser')
 const redirect = require('heroku-ssl-redirect');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/public')));
+const loggedUsers = {}; // change this to memory storage
 
 app.use(redirect());
-
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(cookieParser);
 
 app.get('/', (req, res) => {
-    if (req.cookies['ninkasisum'])
-    {
-        res.setHeader("Content-Type", "text/html");
-        res.sendFile(__dirname + '/views/pages/index.html');
-    } else res.redirect('/login');
+    const file = (loggedUsers[req.headers.cookie])? 'index': 'login';
+    res.setHeader("Content-Type", "text/html")
+    res.sendFile(__dirname + `/views/pages/${file}.html`);
 });
 
 app.get('/login', (req, res) => {
     res.setHeader("Content-Type", "text/html")
     res.sendFile(__dirname + '/views/pages/login.html')
 });
-
 app.get('/shop', (req, res) => {
-    if (req.cookies['ninkasisum'])
-    {
-        res.setHeader("Content-Type", "text/html")
-        res.sendFile(__dirname + '/views/pages/shop.html')
-    } res.redirect('/login');
+    const file = (loggedUsers[req.headers.cookie])? 'shop': 'login';
+    res.setHeader("Content-Type", "text/html")
+    res.sendFile(__dirname + `/views/pages/${file}.html`);
 });
 
 app.post('/api/login', (req, res) => {
     const user = req.body;
-    const cookie = req.cookies['ninkasisum'];
-
-    if (!cookie)
-    {
-        res.cookie('ninkasisum', user['Ea']);
-        req.cookies['ninkasisum'] = user['Ea'];
-    }
-
+    const cookie = user['Ea'];
+    loggedUsers[cookie] = user;
+    res.setHeader('Set-Cookie', cookie);
     res.redirect(`https://${req.host}`);
 });
 
