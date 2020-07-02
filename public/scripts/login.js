00bce66d9175e01b82129eaac6b1e8f1f23955ef
bkkater <biancamedeios.bs10@gmail.com>
2020-07-02 15:05:17 -0300

    fix mask

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -27,7 +27,7 @@ const masks = {
         .replace(/\D/g, '') 
         .replace(/(\d{2})(\d)/, '$1.$2')
         .replace(/(\d{3})(\d)/, '$1.$2')
-        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{4})(\d)/, '$1/$2')
 
         .replace(/(\d{3})(\d{1,2})/, '$1-$2')
         
