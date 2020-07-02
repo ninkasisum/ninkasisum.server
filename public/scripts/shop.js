e51dd859ae7099ab18e654302bb4d419449c17a9
Diogo Junqueira Geraldo <diogojunqueirageraldo@gmail.com>
2020-07-01 21:39:52 -0300

    [improvement] loadUserData

---
+++ public/scripts/shop.js
@@ -0,0 +1,14 @@
+function load() {
+    const config = {
+        "dif-user-name": {
+            prop: 'innerText',
+            path: ['Qt', 'zW']
+        },
+        "dif-user-img": {
+            prop: 'src',
+            path: ['Qt', 'cL']
+        }
+    }
+
+    loadUserData(config);
+}
