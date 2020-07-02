8cd413797821e4f5643bf02dcffebf6e82053363
bkkater <biancamedeios.bs10@gmail.com>
2020-07-02 14:20:44 -0300

    fix sign display

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,20 +1,20 @@
 const loginButton = document.getElementById('login')
 const signButton = document.getElementById('sign-up')
 const loginDisplay = document.getElementById('login-content')
-
-console.log(loginDisplay)
+const signDisplay = document.getElementById('sign-content')
 
 loginButton.addEventListener('click', ()=>{
     loginButton.classList.remove('active')
     loginButton.classList.add('active')
     loginDisplay.classList.remove('none')
     signButton.classList.remove('active')
-    console.log('clicou login')
+    signDisplay.classList.remove('none')
+    signDisplay.classList.add('none')
 })
 signButton.addEventListener('click', ()=>{
     signButton.classList.remove('active')
     signButton.classList.add('active')
     loginButton.classList.remove('active')
     loginDisplay.classList.add('none')
-    console.log('clicou sign')
+    signDisplay.classList.remove('none') 
 })
