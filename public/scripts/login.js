9ecbc4b5d8a72752ead5f84fe29afdedb8ea32f8
bkkater <biancamedeios.bs10@gmail.com>
2020-07-02 13:30:17 -0300

    login test js

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,13 +1,18 @@
 const loginButton = document.getElementById('login')
 const signButton = document.getElementById('sign-in')
 
+console.log(loginButton)
+console.log(signButton)
+
 loginButton.addEventListener('click', ()=>{
     loginButton.classList.remove('active')
     loginButton.classList.add('active')
     signButton.classList.remove('active')
+    console.log('clicou login')
 })
 signButton.addEventListener('click', ()=>{
     signButton.classList.remove('active')
     signButton.classList.add('active')
     loginButton.classList.remove('active')
+    console.log('clicou sign')
 })
