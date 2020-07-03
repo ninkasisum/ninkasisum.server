6088b637b109cf18cfa9be63ca0d644548e8de43
bkkater <biancamedeios.bs10@gmail.com>
2020-07-03 14:32:35 -0300

    first commit shop js

--- public/scripts/shop.js
+++ public/scripts/shop.js
@@ -12,3 +12,36 @@ function load() {
 
     loadUserData(config);
 }
+
+//array de marcas da ambev
+const marcas = [
+    "Adriática",
+    "Antarctica",
+    "Becks",
+    "Bohemia",
+    "Brahma",
+    "Budweiser",
+    "Caracu",
+    "Cervejaria Colorado",
+    "Corona",
+    "Franziskaner",
+    "Goose Island",
+    "Hertog Jan",
+    "Hoegaarden",
+    "Leffe",
+    "Legítima",
+    "Löwenbräu",
+    "Magnífica do Maranhão",
+    "Norteña",
+    "Nossa",
+    "Original",
+    "Patagonia",
+    "Polar",
+    "Quilmes",
+    "Serramalte",
+    "Serrana",
+    "Skol",
+    "Stella Artois",
+    "Três Fidalgas",
+    "Wäls"
+]
