bff1f2ebff4df59e7c3259bdd34568669dd303e9
bkkater <biancamedeios.bs10@gmail.com>
2020-07-03 22:49:44 -0300

    test js add element

--- public/scripts/shop.js
+++ public/scripts/shop.js
@@ -45,3 +45,19 @@ const marcas = [
     "Três Fidalgas",
     "Wäls"
 ]
+
+/*marcas.forEach((marca) =>{
+    document.createElement(input)
+})*/
+
+function adcElemento () { 
+    // cria um novo elemento div 
+    // e dá à ele conteúdo
+    var divNova = document.createElement("div"); 
+    var conteudoNovo = document.createTextNode("Olá, cumprimentos!"); 
+    divNova.appendChild(conteudoNovo); //adiciona o nó de texto à nova div criada 
+  
+    // adiciona o novo elemento criado e seu conteúdo ao DOM 
+    var divAtual = document.getElementById("div1"); 
+    document.body.insertBefore(divNova, divAtual); 
+  }
