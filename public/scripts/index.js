4fff40a505551a25dd39cff8fb2855c2508b813d
bkkater <biancamedeios.bs10@gmail.com>
2020-07-04 18:49:32 -0300

    [remove] text area resize

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -23,21 +23,3 @@ like.addEventListener('click', () => {
         button_like = false
     }
 })
-
-//resize textarea
-var text = document.getElementById('mensagem');
-var textArea = document.getElementById('mensagem-area')
-
-function resize() {
-    text.style.height = 'auto';
-    textArea.style.height = 'auto';
-    text.style.height = text.scrollHeight + 'px';
-    textArea.style.height = (text.scrollHeight + 100) +'px';
-}
-
-['change', 'cut', 'paste', 'drop', 'keydown'].forEach((e) => {
-    text.addEventListener(e, resize)
-})
-
-text.focus();
-text.select();
