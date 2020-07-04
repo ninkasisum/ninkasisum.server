869dfa5ec467ccf0dbfb7139a32e2e0649da89c2
bkkater <biancamedeios.bs10@gmail.com>
2020-07-04 18:57:26 -0300

    [fix] sintax json

--- public/scripts/networking.js
+++ public/scripts/networking.js
@@ -31,7 +31,7 @@ function createGist(url, content) {
 async function loadUserData() {
     return await new Promise((resolve, reject) => {
         fetch(`${window.location.origin}/api/user`, { method: 'POST' })
-            .then(response => response.json())
+            .then(response => response.body.json())
             .then(resolve)
     })
 }
