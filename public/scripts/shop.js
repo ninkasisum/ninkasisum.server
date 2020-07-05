181fad5d71016925771f3341bfd043eb86780905
Diogo Junqueira Geraldo <diogojunqueirageraldo@gmail.com>
2020-07-05 01:50:38 -0300

    [add] dynamic generate cards onload - bare metal

--- public/scripts/shop.js
+++ public/scripts/shop.js
@@ -1,21 +1,6 @@
-const data = {};
-
 function load() {
-    const config = {
-        "dif-user-name": {
-            prop: 'innerText',
-            path: ['Qt', 'zW']
-        },
-        "dif-user-img": {
-            prop: 'src',
-            path: ['Qt', 'cL']
-        }
-    }
-
     loadUserData(config);
-    jsonLoadResource("shop").then((json) => {
-        data.shop = json;
-    })
+    jsonLoadResource("shop").then(loadCards);
     filterinput()
 }
 
@@ -31,14 +16,97 @@ function filterinput() {
     const input = document.getElementById("input-ft")
 
     input.addEventListener('input', (e) => {
-        document.querySelectorAll(".filter-data").forEach((label)=>{
+        document.querySelectorAll(".filter-data").forEach((label) => {
             if (label.innerHTML.toLowerCase().search(input.value.toLowerCase()) === 0) {
-                label.parentElement.style.display = "block";               
+                label.parentElement.style.display = "block";
             }
-            else{
+            else {
                 label.parentElement.style.display = "none";
             }
         })
 
     })
+}
+
+function loadCards(data) {
+    const { modelos, categorias, teores, logo_urls, image_urls } = data;
+
+    for (let i = 0; i < modelos.length; i++)
+    {
+        const card = document.createElement('div');
+        card.setAttribute('class', 'card');
+
+        // create logo
+        const logo = document.createElement('div');
+        logo.setAttribute('class', 'beer-logo');
+
+        const imgLogo = document.createElement('img');
+        imgLogo.setAttribute('src', logo_urls[i]);
+
+        logo.appendChild(img);
+        card.appendChild(logo);
+        // end create logo
+
+        // create content
+        const content = document.createElement('div');
+        content.setAttribute('class', 'content');
+
+        // create img
+        const imgContent = document.createElement('img');
+        imgContent.setAttribute('src', image_urls[i]);
+        imgContent.setAttribute('class', 'beer');
+        content.appendChild(imgContent);
+        // end create img
+
+        // create info
+        const info = document.createElement('div');
+        info.setAttribute('class', 'inform');
+
+        const categoria = {
+            element: document.createElement('h3'),
+            text: document.createElement('h4')
+        }
+
+        categoria.element.appendChild(document.createTextNode('Categoria'));
+        categoria.text.appendChild(document.createTextNode(categorias[i]));
+        info.appendChild(categoria.element);
+        info.appendChild(categoria.text);
+
+        const teorAlcoolico = {
+            element: document.createElement('h3'),
+            text: document.createElement('h4')
+        }
+
+        teorAlcoolico.element.appendChild(document.createTextNode('Teor Alcoólico'));
+        teorAlcoolico.text.appendChild(document.createTextNode(teores[i]));
+
+        info.appendChild(teorAlcoolico.element);
+        info.appendChild(teorAlcoolico.text);
+
+        content.appendChild(info);
+        // end create info
+
+        // create buttons
+        const buttons = document.createElement('div');
+        buttons.setAttribute('class', 'button-card');
+
+        const buttonAdd = document.createElement('button');
+        buttonAdd.appendChild(document.createTextNode('Adicionar ao Carrinho'));
+
+        const icon = document.createElement('i');
+        icon.setAttribute('class', 'fas fa-shopping-basket');
+        buttonAdd.appendChild(icon);
+        
+        const buttonBuy = document.createElement('button');
+        buttonBuy.setAttribute('class', 'dif-button');
+        buttonBuy.appendChild(document.createTextNode('COMPRAR AGORA'));
+
+        buttons.appendChild(buttonAdd);
+        buttons.appendChild(buttonBuy);
+        content.appendChild(buttons);
+        // end create buttons
+
+        card.appendChild(content);
+        // end create content
+    }
 }
